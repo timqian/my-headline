a15600b623a2d6fe4c111d11a34112b4ad48b059
timqian <timqian92@qq.com>
2016-04-06 17:06:46 +0800

    update view

--- client/components/Box/index.js
+++ client/components/Box/index.js
@@ -1,17 +1,12 @@
 
-import React, { Component } from 'react'
-import classnames from 'classnames'
+import React from 'react'
 import style from './style.css'
-
 
 function Box({ site, links }) {
   return (
     <div className={style.box}>
-      <div className={style.site}>{site}</div>     
-
-      {
-        links.map( link => <a className={style.link} href={link.url}> {link.title} </a> )
-      }
+      <div className={style.site}>{site}</div>  
+      { links.map( link => <a className={style.link} href={link.url}> {link.title} </a> ) }
 
     </div>
   )
---
+++ client/components/Header/index.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import style from './style.css'
+
+function Header() {
+  return (
+    <header className={style.header}>
+      2016-4-6
+    </header>
+  )
+}
+
+export default Header
