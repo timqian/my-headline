01cfb2c4e7752e30190b36d8b2950ddf44ae701e
timqian <timqian92@qq.com>
2016-08-16 16:47:48 +0800

    allow empty page

--- src/utils/getHeadlines.js
+++ src/utils/getHeadlines.js
@@ -4,16 +4,19 @@ import notie from 'corner-notie'
 import { dataUrl } from '../config'
 
 async function getHeadlines(timeSplat) {
-  const { data } = await axios.get(`${dataUrl}/${timeSplat}.json`, {timeout: 7000})
-    .catch(res => {
-      notie('Seems no data there~' , {
-        type: 'warning', // info | warning | success | danger
-        autoHide: true,
-        timeout: 3000,
-        position: 'bottom-center',
-        width: 250
-      })
+  let data = {}
+  try {
+    data = ( await axios.get(`${dataUrl}/${timeSplat}.json`, {timeout: 7000}) ).data
+  } catch (error) {
+    notie('Seems no data there~' , {
+      type: 'warning', // info | warning | success | danger
+      autoHide: true,
+      timeout: 3000,
+      position: 'bottom-center',
+      width: 250
     })
+  }
+
   return data
 }
 
